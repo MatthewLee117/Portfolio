d3117f0f973d56caf41d503772cd866b3e1522f9
Matthew Lee <91569731+MatthewLee117@users.noreply.github.com>
2026-04-17 20:41:17 -0700

    javas

--- global.js
+++ global.js
@@ -4,9 +4,42 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-const navLinks = $$("nav a");
-const currentLink = navLinks.find(
-  (a) => a.host === location.host && a.pathname === location.pathname
-);
+const pages = [
+  { url: "", title: "Home" },
+  { url: "projects/", title: "Projects" },
+  { url: "contact/", title: "Contact" },
+  { url: "resume/?v=2", title: "Resume" },
+  { url: "https://github.com/matthewlee", title: "GitHub" },
+];
 
-currentLink?.classList.add("current");
+const BASE_PATH =
+  location.hostname === "localhost" || location.hostname === "127.0.0.1"
+    ? "/"
+    : "/Portfolio/";
+
+const nav = document.createElement("nav");
+document.body.prepend(nav);
+
+for (const p of pages) {
+  let url = p.url;
+  const title = p.title;
+
+  if (!url.startsWith("http")) {
+    url = BASE_PATH + url;
+  }
+
+  const a = document.createElement("a");
+  a.href = url;
+  a.textContent = title;
+
+  if (a.host === location.host && a.pathname === location.pathname) {
+    a.classList.add("current");
+  }
+
+  if (a.host !== location.host) {
+    a.target = "_blank";
+    a.rel = "noopener noreferrer";
+  }
+
+  nav.append(a);
+}
